a073233381c6def90fba72857ef76169576b53d7
annisa sholehatul jannah <annisa@MacBook-Air-annisa.local>
2017-03-22 12:45:45 +0700

    1st commmit

---
+++ logout.php
@@ -0,0 +1,15 @@
+Buat file logout.php lalu isi dengan script berikut
+<?php  
+    // Lampirkan db dan User
+    require_once "db.php";
+    require_once "User.php";
+
+    // Buat object user
+    $user = new User($db);
+
+    // Logout! hapus session user
+    $user->logout();
+
+    // Redirect ke login
+    header('location: login.php');
+ ?>
